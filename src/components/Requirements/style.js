5bcc28c3add9be0e0d5df8bb4f322b5beb53b287
julia <julia@debian.ad.vsu>
2022-04-19 16:16:12 +0300

    style(style.js): added svg format

--- src/components/Requirements/style.js
+++ src/components/Requirements/style.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const Button = styled.button`
   background-color: black;
-  background: url('@/assets/images/error');
+  background: url('@/assets/images/error.svg');
 `;
