af5c0488d3c3f1fb0a36f1a940108e071c4b5222
julia <julia@debian.ad.vsu>
2022-04-27 18:14:12 +0300

    feat(ToastPertal): testing method add

--- src/containers/ToastPortal/ToastPortal.jsx
+++ src/containers/ToastPortal/ToastPortal.jsx
@@ -1,24 +1,24 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useImperativeHandle, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { useToastPortal } from '@/hooks';
-import { Toast } from '@/components/Toast';
-// import { service } from '@/services/ToastService';
+import { useToastPortal, useAutoCloseToast } from '../../hooks';
+import { Toast } from '../../components/Toast';
+import { ToastService } from '../../services/ToastService';
+
+const service = ToastService.getInstance();
 
 export const ToastPortal = React.forwardRef(({ location }, ref) => {
-  const [toasts] = useState([]);
+  const [toasts, setToasts] = useState([]);
   const { loaded, portalId } = useToastPortal(location);
 
-  /* function remove(id) {
+  function remove(id) {
     return () => setToasts(service.removeToast(id));
-  } */
-
-  // useAutoCloseToast(toasts, remove);
+  }
+  useAutoCloseToast(toasts, remove);
 
   useImperativeHandle(ref, () => ({
-    add: () => {
-      console.log('add');
-      // if (toasts.length !== 3) setToasts(service.addToast(prop));
+    add: (prop) => {
+      if (toasts.length !== 3) setToasts(service.addToast(prop));
     }
   }));
 
@@ -26,7 +26,7 @@ export const ToastPortal = React.forwardRef(({ location }, ref) => {
     ? ReactDOM.createPortal(
         <div>
           {toasts.map((t) => (
-            <Toast {...t} />
+            <Toast key={t.id} {...t} onClose={remove(t.id)} />
           ))}
         </div>,
         document.getElementById(portalId)
