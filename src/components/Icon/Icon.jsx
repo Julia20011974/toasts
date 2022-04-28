32b1f896d5ae9fe1687f55e053ca3aadc04fe174
julia <julia@debian.ad.vsu>
2022-04-28 11:29:15 +0300

    1.1.14

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image } from './style';
 import { getImage } from '@/helpers/getImage';
-import question from '@/images/question.svg';
+import question from '../../images/question.svg';
 
 export function Icon({ img }) {
   console.log(getImage(img));
