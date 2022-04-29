edee29770d2529d390fa1251f249e757005d7dad
julia <julia@debian.ad.vsu>
2022-04-29 14:14:58 +0300

    added webpack

--- src/hooks/useAutoCloseToast.js
+++ src/hooks/useAutoCloseToast.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+
 import { DELAY } from '@/constants';
 
 export const useAutoCloseToast = (toasts, remove) => {
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, 'build')
+  },
+  resolve: {
+    modules: [__dirname, 'src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    alias: {
+      '@/': path.join(__dirname, './src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: require.resolve('babel-loader')
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
